7e5132d79d143ae8a49eadb535ef0f06292cdbe4
drk-yz <dericklevif@gmail.com>
2026-09-19 13:33:02 -0300

    Add JavaScript functions for various calculations

---
+++ javascript/Atividade 2.js
@@ -0,0 +1,89 @@
+// // Questão 1
+// function somarNumeros (n1,n2){
+//     console.log(n1+n2)
+// }
+// somarNumeros(4,6)
+
+// // Questão 2
+// function subtrairNumeros (n1,n2){
+//     console.log(n1-n2)
+// }
+// subtrairNumeros(6,4)
+
+// // Questão 3
+// function multiplicarNumeros (n1,n2){
+//     console.log(n1*n2)
+// }
+// multiplicarNumeros(6,4)
+
+// // Questao 4 
+// function dividirNumeros (n1,n2){
+//     console.log(n1/n2)
+// }
+// dividirNumeros(20,4)
+
+// // Questão 5
+// function restoNumeros (n1,n2){
+//     console.log(n1%n2)
+// }
+// restoNumeros(21,4)
+
+// // Questão 6
+// function dobroNumero (n1){
+//     console.log(n1*2)
+// }
+// dobroNumero(2)
+
+// // Questão 7
+// function metadeNumero (n1){
+//     console.log(n1/2)
+// }
+// metadeNumero(2)
+
+// // Questão 8
+// function areaRetangulo (n1,n2){
+//     console.log(n1*n2)
+// }
+// areaRetangulo(10,5)
+
+// // Questão 9
+// function media(n1,n2,n3){
+//     console.log( (n1+n2+n3)/3 )
+// }
+// media(8,10,9)
+
+// // Questão 10
+// function perimetroQuadrado(n1){
+//     console.log(n1*4)
+// }
+// perimetroQuadrado(5)
+
+// // Questão 11
+// function conversao (n1){
+//     console.log(n1*60)
+// }
+// conversao(3)
+
+// // Questão 12
+// function salarioMensal (n1,n2){
+//     console.log(n1*n2)
+// }
+// salarioMensal(9,180)
+
+// // Questão 13
+// function cm_m (n1){
+//     console.log(n1/100)
+// }
+// cm_m(150)
+
+// // Questao 14
+// function valorTotal(n1,n2,n3){
+//     console.log(n1+n2+n3)
+// }
+// valorTotal(100,200,400)
+
+// // Questão 15
+// function idadeDias(n1){
+//     console.log(n1*365)
+// }
+// idadeDias(20)
